07e3c620a57ea7e0e4cd9085fd4b55e810786929
reiva5 <jehiannormansaviero@gmail.com>
2018-04-02 22:04:40 +0700

    Add implementation of car.cpp

---
+++ src/car.cpp
@@ -0,0 +1,44 @@
+#include "car.h"
+
+Car::Car(const string& id, const string& colour){
+	this->colour = colour;
+	this->id = id;
+}
+
+Car::Car(const Car& otherCar){
+	this->colour = otherCar.colour;
+	this->id = otherCar.id;
+}
+
+Car& Car::operator=(const Car& otherCar){
+	this->colour = otherCar.colour;
+	this->id = otherCar.id;
+}
+
+string Car::getColour() const {
+	return this->colour;
+}
+
+string Car::getId() const {
+	return this->id;
+}
+
+void Car::setColour(string colour){
+	this->colour = colour;
+}
+
+void Car::setId(string id){
+	this->id = id;
+}
+
+bool operator<(const Car& A, const Car& B){
+	return A.id < B.id || (A.id == B.id && A.colour < B.colour);
+}
+
+bool operator>(const Car& A, const Car& B){
+	return A.id > B.id || (A.id == B.id && A.colour > B.colour);
+}
+
+bool operator==(const Car& A, const Car& B){
+	return A.id == B.id && A.colour == B.colour;
+}
